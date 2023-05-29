00959fa96887a497125a72a13efdd10530c91118
AABB111 <dikaprio-1986@mail.ru>
2023-05-29 22:58:38 +0300

    fc

---
+++ DSA/DsaSignature.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+namespace DSA
+{
+    internal class DsaSignature
+    {
+        public BigInteger Q;
+        public BigInteger P;
+        public BigInteger Y;
+        private BigInteger G;
+        private BigInteger X;
+        public DsaSignature()
+        {
+
+        }
+        public DsaSignature(BigInteger q)
+        {
+            if (!MillerRabinTest(q, 10))
+            {
+                throw new Exception("q должно быть простое");
+            }
+            
+            Q = q;
+            P = GetP();
+            G = GetG();
+            X = GetX();
+            Y = GetY();
+        }
+        private BigInteger GetP()
+        {
+            BigInteger p = Q + 1;
+            while (true)
+            {
+                if (MillerRabinTest(p, 10) && (p - 1) % Q == 0)
+                    return p;
+                else
+                    p++;
+            }
+        }
+        private BigInteger GetG()
+        {
+            BigInteger h = 2;
+            BigInteger g;
+            do
+            {
+                g = BigInteger.ModPow(h, (P - 1) / Q, P);
+            }
+            while (g <= 1) ;
+            return g;
+        }
+        private BigInteger GetY()
+        {
+            return BigInteger.ModPow(G, X, P);
+        }
+        private BigInteger GetX()
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            Random random = new Random();
+            byte[] _a = new byte[P.ToByteArray().LongLength];
+            BigInteger x;
+
+            do
+            {
+                random.NextBytes(_a);
+                x = new BigInteger(_a);
+            }
+            while (x <= 0 || x >= Q);
+
+            return x;
+        }
+        public (string,BigInteger,BigInteger) Signature(string str)
+        {
+            BigInteger r;
+            BigInteger s;
+            do
+            {
+                BigInteger k = 2;
+                BigInteger h = str.GetHashCode();
+                r = BigInteger.ModPow(G,k,P) % Q;
+                s = Mod((Invmod(k,Q) * (h + X * r)),Q); 
+
+            } while (r == 0 || s == 0);
+            return (str, r, s);
+        }
+        public bool CheckSignature((string, BigInteger, BigInteger) sms,(BigInteger, BigInteger,BigInteger) publicKey)
+        {
+            string message = sms.Item1;
+            BigInteger r = sms.Item2;
+            BigInteger s = sms.Item3;
+            BigInteger P = publicKey.Item1;
+            BigInteger Q = publicKey.Item2;
+            BigInteger Y = publicKey.Item3;
+            BigInteger w = Invmod(s,Q);
+            BigInteger u1 = Mod((message.GetHashCode() * w) , Q);
+            BigInteger u2 = Mod((r * w), Q);
+            BigInteger v = BigInteger.Pow(G, (int)u1) * BigInteger.Pow(Y, (int)u2) % P % Q;
+            if (r == v)
+                return true;
+            else
+                return false;
+        }
+        private bool MillerRabinTest(BigInteger n, int k)
+        {
+            if (n == 2 || n == 3)
+                return true;
+            if (n < 2 || n % 2 == 0)
+                return false;
+
+            BigInteger t = n - 1;
+            int s = 0;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s++;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                byte[] _a = new byte[n.ToByteArray().LongLength];
+                BigInteger a;
+
+                do
+                {
+                    rng.GetBytes(_a);
+                    a = new BigInteger(_a);
+                }
+                while (a < 2 || a >= n - 2);
+
+                BigInteger x = BigInteger.ModPow(a, t, n);
+
+                if (x == 1 || x == n - 1)
+                    continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+
+                if (x != n - 1)
+                    return false;
+            }
+            return true;
+        }
+        private (BigInteger, BigInteger, BigInteger) GCD(BigInteger a, BigInteger b)
+        {
+            if (a == 0)
+                return (b, 0, 1);
+            (BigInteger gcd, BigInteger x, BigInteger y) = GCD(b % a, a);
+            return (gcd, y - (b / a) * x, x);
+        }
+        private BigInteger Invmod(BigInteger a, BigInteger m)
+        {
+            if (a < 0)
+                a *= -1;
+            if (m < 0)
+                m *= -1;
+            (BigInteger g, BigInteger x, BigInteger y) = GCD(a, m);
+            return g > 1 ? 0 : Mod(x,m); 
+        }
+        private BigInteger Mod(BigInteger a, BigInteger m)
+        {
+            return a < 0 ? m - (-a) % m : a % m;
+        }
+    }
+}
---
+++ DSA/MainContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace DSA
+{
+    internal class MainContext : Context
+    {
+        private Command? signatureCommand;
+        private Command? checkCommand;
+        private DsaSignature? dsa;
+        private string? r;
+        private string? s;
+        private string? p;
+        private string? qcheck;
+        private string? y;
+        private string message;
+        public uint Q { get; set; }
+        public string? SMS { get; set; }
+        public string Message { get { return message; } set { message = value; OnPropertyChanged(); } }
+        public string P { get { return p; } set { p = value; OnPropertyChanged(); } }
+        public string Qcheck { get { return qcheck; } set { qcheck = value; OnPropertyChanged(); } }
+        public string Y { get { return y; } set { y = value; OnPropertyChanged(); } }
+        public string R { get { return r; } set { r = value;OnPropertyChanged(); } }
+        public string S { get { return s; } set { s = value; OnPropertyChanged(); } }
+        public Command SignatureCommand
+        {
+            get
+            {
+                return signatureCommand ??= new Command((obj) =>
+                {
+                    try
+                    {
+                        if (this.dsa?.Q != Q || this.dsa == null)
+                        {
+                            DsaSignature dsa = new(Q);
+                            this.dsa = dsa;
+                        }
+                        var smsWithSignature = dsa.Signature(SMS);
+                        R = smsWithSignature.Item2.ToString();
+                        S = smsWithSignature.Item3.ToString();
+                        P = dsa.P.ToString();
+                        Qcheck = dsa.Q.ToString();
+                        Y = dsa.Y.ToString();
+                        Message = SMS;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                });
+            }
+        }
+        public Command CheckCommand
+        {
+            get
+            {
+                return checkCommand ??= new Command((obj) =>
+                {
+                    try
+                    {
+                        _ = (bool)(dsa?.CheckSignature(new(Message, BigInteger.Parse(R), BigInteger.Parse(S)), new(BigInteger.Parse(P), BigInteger.Parse(Qcheck), BigInteger.Parse(Y)))) ? MessageBox.Show("Это оригинал") : MessageBox.Show("Подделка");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    
+                });
+            }
+        }
+    }
+}
